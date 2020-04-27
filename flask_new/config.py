73866624c71e2471a0bbd608a9c37345779e7944
RIO <riogokul29@gmail.com>
2020-04-28 01:22:44 +0530

    Fitsrt commit

---
+++ flask_new/config.py
@@ -0,0 +1,10 @@
+
+
+class Config:
+    SECRET_KEY = 'ea58010d37db999791db19402a4355af'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'gokul.suresh.291196@gmail.com'
+    MAIL_PASSWORD = 'greatgokul1!'
---
+++ run.py
@@ -0,0 +1,8 @@
+from flask_new import create_app
+
+app= create_app()
+
+if __name__ =='__main__':
+    app.run(debug=True) 
+
+
